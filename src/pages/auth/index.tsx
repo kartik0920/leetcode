1fc2b79b684b9d16ddffb9e42e2dff241831b0f9
kartik0920 <kartik0920@gmail.com>
2024-12-11 19:52:04 +0530

    Creating UI

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import Navbar from "@/components/Navbar/Navbar";
-import { useRecoilValue } from "recoil";
-import { authModalState } from "@/atoms/authModalAtom";
 import AuthModal from "@/components/Modals/AuthModal";
-
 type indexProps = {};
 const AppPage: React.FC<indexProps> = () => {
-  // const authModal = useRecoilValue(authModalState);
-  console.log(useRecoilValue(authModalState));
   return (
     <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
       <div className="max-w-7xl mx-auto">
@@ -16,6 +11,7 @@ const AppPage: React.FC<indexProps> = () => {
           <h1 className="text-2xl">Hello this is LeetCode Clone</h1>
           <small className="color-white">Created by @kartik0920</small>
         </div>
+        <AuthModal />
       </div>
     </div>
   );
